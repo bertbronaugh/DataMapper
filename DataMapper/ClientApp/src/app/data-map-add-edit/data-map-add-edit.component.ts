2f1609cd4900ef682c204b17f62d4f36a8fc73a9
Bert.Bronaugh <bert.bronaugh@tylertech.com>
2021-01-15 22:47:59 -0600

    Day 20

--- DataMapper/ClientApp/src/app/data-map-add-edit/data-map-add-edit.component.ts
+++ DataMapper/ClientApp/src/app/data-map-add-edit/data-map-add-edit.component.ts
@@ -12,39 +12,77 @@ import { DataMap } from '../models/datamap';
 export class DataMapAddEditComponent implements OnInit {
   form: FormGroup;
   actionType: string;
-  formTitle: string;
-  formBody: string;
-  postId: number;
+
+  // My fields
+  dataMapId: number;
+  formCustno: number;
+  formState: string;
+  formTable: string;
+  formData1Value: string;
+  formData1Description: string;
+  formDataIntValue: number;
+  formData2Value: string;
+  formData2Description: string;
+  created: Date;
+  updated: Date;
+
+  // Old fields
+  // formTitle: string;
+  // formBody: string;
+  // postId: number;
+
   errorMessage: any;
   existingDataMap: DataMap;
 
   constructor(private DataMapService: DataMapService, private formBuilder: FormBuilder, private avRoute: ActivatedRoute, private router: Router) {
     const idParam = 'id';
     this.actionType = 'Add';
-    this.formTitle = 'title';
-    this.formBody = 'body';
+    this.formCustno = 99;
+    this.formState = 'TX';
+    this.formTable = 'table';
+    this.formData1Value = 'data1Value';
+    this.formData1Description = '';
+    this.formDataIntValue = 0;
+    this.formData2Value = 'data2Value';
+    this.formData2Description = ''
+    // this.formTitle = 'title';
+    // this.formBody = 'body';
     if (this.avRoute.snapshot.params[idParam]) {
-      this.postId = this.avRoute.snapshot.params[idParam];
+      this.dataMapId = this.avRoute.snapshot.params[idParam];
     }
 
     this.form = this.formBuilder.group(
       {
-        postId: 0,
-        title: ['', [Validators.required]],
-        body: ['', [Validators.required]],
+        dataMapId: 0,
+        formCustno: [99, [Validators.required]],
+        formState: ['', [Validators.required]],
+        formTable: ['', [Validators.required]],
+        formData1Value: ['', [Validators.required]],
+        formData1Description: ['', [Validators]],
+        formDataIntValue: [0, [Validators.required]],
+        formData2Value: ['', [Validators.required]],
+        formData2Description: ['', [Validators]]
+        // title: ['', [Validators.required]],
+        // body: ['', [Validators.required]],
       }
     )
   }
 
   ngOnInit() {
 
-    if (this.postId > 0) {
+    if (this.dataMapId > 0) {
       this.actionType = 'Edit';
-      this.DataMapService.getDataMap(this.postId)
+      this.DataMapService.getDataMap(this.dataMapId)
         .subscribe(data => (
           this.existingDataMap = data,
-          this.form.controls[this.formTitle].setValue(data.title),
-          this.form.controls[this.formBody].setValue(data.body)
+          this.form.controls[this.formCustno].setValue(data.custno),
+          this.form.controls[this.formState].setValue(data.state),
+          this.form.controls[this.formTable].setValue(data.table),
+          this.form.controls[this.formData1Value].setValue(data.data1Value),
+          this.form.controls[this.formData1Description].setValue(data.data1Description),
+          this.form.controls[this.formDataIntValue].setValue(data.dataIntValue),
+          this.form.controls[this.formData2Value].setValue(data.data2Value),
+          this.form.controls[this.formData2Description].setValue(data.data2Description)
         ));
     }
   }
@@ -56,27 +94,50 @@ export class DataMapAddEditComponent implements OnInit {
 
     if (this.actionType === 'Add') {
       let dataMap: DataMap = {
-        dt: new Date(),
-        creator: 'Martin',
-        title: this.form.get(this.formTitle).value,
-        body: this.form.get(this.formBody).value
+        created: new Date(),
+        updated: new Date(),
+        custno: this.form.get(this.formCustno.toString()).value,
+        state: this.form.get(this.formState).value,
+        table: this.form.get(this.formTable).value,
+        data1Value: this.form.get(this.formData1Value).value,
+        data1Description: this.form.get(this.formData1Description).value,
+        dataIntValue: this.form.get(this.formDataIntValue.toString()).value,
+        data2Value: this.form.get(this.formData2Value).value,
+        data2Description: this.form.get(this.formData2Description).value
+
+        // dt: new Date(),
+        // creator: 'Martin',
+        // title: this.form.get(this.formTitle).value,
+        // body: this.form.get(this.formBody).value
       };
 
       this.DataMapService.saveDataMap(dataMap)
         .subscribe((data) => {
-          this.router.navigate(['/blogpost', data.postId]);
+          this.router.navigate(['/datamap', data.dataMapId]);
         });
     }
 
     if (this.actionType === 'Edit') {
       let dataMap: DataMap = {
-        postId: this.existingDataMap.postId,
-        dt: this.existingDataMap.dt,
-        creator: this.existingDataMap.creator,
-        title: this.form.get(this.formTitle).value,
-        body: this.form.get(this.formBody).value
+        dataMapId: this.existingDataMap.dataMapId,
+        created: this.existingDataMap.created,
+        updated: new Date(),
+        
+        custno: this.form.get(this.formCustno.toString()).value,
+        state: this.form.get(this.formState).value,
+        table: this.form.get(this.formTable).value,
+        data1Value: this.form.get(this.formData1Value).value,
+        data1Description: this.form.get(this.formData1Description).value,
+        dataIntValue: this.form.get(this.formDataIntValue.toString()).value,
+        data2Value: this.form.get(this.formData2Value).value,
+        data2Description: this.form.get(this.formData2Description).value
+        // postId: this.existingDataMap.postId,
+        // dt: this.existingDataMap.dt,
+        // creator: this.existingDataMap.creator,
+        // title: this.form.get(this.formTitle).value,
+        // body: this.form.get(this.formBody).value
       };
-      this.DataMapService.updateDataMap(dataMap.postId, dataMap)
+      this.DataMapService.updateDataMap(dataMap.dataMapId, dataMap)
         .subscribe((data) => {
           this.router.navigate([this.router.url]);
         });
@@ -87,6 +148,13 @@ export class DataMapAddEditComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
-  get title() { return this.form.get(this.formTitle); }
-  get body() { return this.form.get(this.formBody); }
+  get custno() { return this.form.get(this.formCustno.toString()); }
+  get state() { return this.form.get(this.formState); }
+  get table() { return this.form.get(this.formTable); }
+  get data1Value() { return this.form.get(this.formData1Value); }
+  get data1Description() { return this.form.get(this.formData1Description); }
+  get dataIntValue() { return this.form.get(this.formDataIntValue.toString()); }
+  get data2Value() { return this.form.get(this.formData2Value); }
+  get data2Description() { return this.form.get(this.formData2Description); }
+
 }
